63fcc0a37308e5344d6dc0765a95c522049a82f3
grace <2094863012@qq.com>
2024-10-19 09:51:31 +0800

    将timer封装

--- timer/main.cpp
+++ timer/main.cpp
@@ -1,35 +1,40 @@
 #include <chrono>
 #include <iostream>
 
-std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
-long totduration;
+class Timer {
+public:
+    void start() {
+        start_time = std::chrono::high_resolution_clock::now();
+    }
 
-void start() {
-    start_time = std::chrono::system_clock::now();
-}
+    void stop() {
+        auto end_time = std::chrono::high_resolution_clock::now();
+        totduration += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+    }
 
-void stop() {
-    auto end_time = std::chrono::system_clock::now();
-    totduration += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
-}
+    double duration() const {
+        return static_cast<double>(totduration) * std::chrono::nanoseconds::period::num / std::chrono::nanoseconds::period::den;
+    }
 
-double duration() {
-    return (double) totduration * std::chrono::nanoseconds::period::num / std::chrono::nanoseconds::period::den;
-}
+    void show() const {
+        std::cout << " took " << duration() << " seconds\n";
+    }
 
-void show() {
-    std::cout << " took " << duration() << " seconds\n";  // Change this line
-}
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+    long totduration = 0;
+};
 
-const int N = 1e9;
+const int N = 1e6;
 int a[N];
 
 int main() {
-    start();
+    Timer timer;
+    timer.start();
     for (int i = 0; i < N; ++i) {
         a[i] = i;
     }
-    stop();
-    show();
+    timer.stop();
+    timer.show();
     return 0;
 }
